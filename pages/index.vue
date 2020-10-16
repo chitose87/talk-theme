1db071bb34d02206dc93acdf2c975fe913d551a7
chitose87 <chitose87@gmail.com>
2020-10-16 11:10:34 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  div
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from "~/node_modules/nuxt-property-decorator";
+
+  @Component({
+    components: {}
+  })
+  export default class Index extends Vue {
+  }
+</script>
+
+<style lang="scss">
+
+</style>
